0234f7ad9e26dd8f477c1453d66503fb5d9537f1
ksirkesalo <kalle.sirkesalo@eficode.com>
2014-12-15 13:19:11 +0200

    minor fixes

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,4 @@ sudo yum install -y make gcc gcc-c+ protobuf libxml2-devel zeromq libaio
 sudo yum install -y R
 cd /vagrant
 sudo rpm -Uvh vertica-distributedR-0.7.0-0.1.el6.x86_64.rpm
-su vagrant
-cd $HOME
-ssh-keygen -t rsa -f vagrant
-cat /home/vagrant/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
-ssh -oStrictHostKeyChecking=no vagrant@127.0.0.1
+
